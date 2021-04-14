43b977ec8673b961c4c08ca912340549565767d1
josephkim1004 <78059383+zmgkgkt1004@users.noreply.github.com>
2021-04-13 20:01:49 -0500

    js
    change name in index.html and add js for the page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function init(){
 //add your javascrip between these two lines of code
- 
 
-
-
-
-
+	document.getElementById("entrybutton").addEventListener("click", function()){
+		var output = "Joseph Kim: " + document.getElementById("entryinput").value;
+		alert(output);
+		document.getElementById("textoutput").innerHTML = document.getElementById("entryinput").value;
+	}
 
 
 window.addEventListener('load', init);
