586ef75eb2070de7032acff4c8bc1e115230489a
josephkim1004 <78059383+zmgkgkt1004@users.noreply.github.com>
2021-04-13 20:08:44 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
 
-	document.getElementById("entrybutton").addEventListener("click", function()){
-		var output = "Joseph Kim: " + document.getElementById("entryinput").value;
+	document.getElementById("entrybutton").addEventListener("click", function(){
+		var inputText = document.getElementById("entryinput").value
+		var output = "Joseph Kim: " + inputText;
 		alert(output);
-		document.getElementById("textoutput").innerHTML = document.getElementById("entryinput").value;
-	}
+		document.getElementById("textoutput").innerHTML = inputText;
+	});
+}
 
 
 window.addEventListener('load', init);
